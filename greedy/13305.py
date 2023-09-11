831cc2514318f53a53b1be0a996a7bad217f0ba4
Gyeongbin <tarabin49@gmail.com>
2023-09-11 23:37:50 +0900

    [13305]주유소 문제 풀이

--- greedy/13305.py
+++ greedy/13305.py
@@ -1,1 +1,21 @@
 import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+road = list(map(int,input().split()))
+value = list(map(int,input().split()))
+
+# print(N)
+# print(road_len)
+# print(value)
+
+cost = value[0]
+total_cost = road[0] * cost
+for i in range(1, N-1) :
+    if value[i] < cost :
+        cost = value[i]
+        total_cost += (road[i] * cost)
+    else :
+        total_cost += (road[i]* cost)
+
+print(total_cost)
